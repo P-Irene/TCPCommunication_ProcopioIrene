e56a17ab6b2aab23e137c92cb9548100033d58a6
P-Irene <procopioirene1@gmail.com>
2025-02-13 19:02:58 +0100

    Aggiunta dei metodi leggi() e scrivi() + aggiunta del colore BLU per evidenziare il Data Socket

--- src/tcpcommunication/Client.java
+++ src/tcpcommunication/Client.java
@@ -5,9 +5,13 @@
 package tcpcommunication;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,6 +23,11 @@ public class Client {
     String nome;
     String colore;
     Socket socket;
+    InputStream is;
+    OutputStream os;
+    String messaggioIn, messaggioOut;
+    Scanner streamIn = null;
+    PrintWriter streamOut = null;
     
     public Client(String nome){
         this.nome = nome;
@@ -27,16 +36,17 @@ public class Client {
     public Client (String nome, String colore){
         this.nome = nome;
         this.colore = colore;
+        
     }
     
     public void connetti(String nomeServer, int portaServer){
         try {
             socket = new Socket(nomeServer, portaServer);
-            System.out.println("1)Connessione avvenuta con il server");
+            System.out.println(colore + "2)Connessione avvenuta con il server");
         } catch (ConnectException ex){
-            System.err.println("Errore server non in ascolto.");
+            System.err.println("Errore server non in ascolto");
         } catch (UnknownHostException ex){
-            System.err.println("Errore risoluzione del nome del server.");
+            System.err.println("Errore risoluzione del nome del server");
         }catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Errore nella fase di connessione con il server");
@@ -44,20 +54,42 @@ public class Client {
     }
     
     public void scrivi(){
-        
+        if(socket != null)
+            try {
+                os = socket.getOutputStream();
+                streamOut = new PrintWriter(os);
+                streamOut.flush();
+
+                messaggioOut = "Eccomi!";
+                streamOut.println(messaggioOut);
+                streamOut.flush();
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+                System.err.println("Errore di I/O!");
+            }
     }
     
     public void leggi(){
-        
+        try {
+            is = socket.getInputStream();
+            streamIn = new Scanner(is);
+            
+            messaggioIn = streamIn.nextLine();
+            System.out.println("4)Messaggio del server: " + messaggioIn);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Errore di I/O!");
+        }
     }
     
     public void chiudi(){
         if(socket != null)  //NullPointerException (NON CONTROLLATA)
             try {
                 socket.close();
-                System.out.println("4)Chiusura della connessione con il server");
+                System.out.println(Server.BLU + "5)Chiusura della connessione con il server");
             } catch (IOException ex) {
                 Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+                System.err.println("Errore di chiusura della connessione con il server");
             }
     }
 }
