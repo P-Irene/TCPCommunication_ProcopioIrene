07a2f89362d2ab69d47b386de7003ab1e06eb99a
P-Irene <procopioirene1@gmail.com>
2025-02-14 16:56:27 +0100

    Aggiunta dei colori come costanti

--- src/tcpcommunication/MainClient.java
+++ src/tcpcommunication/MainClient.java
@@ -9,16 +9,22 @@ package tcpcommunication;
  * @author Irene
  */
 public class MainClient {
-
+    public static final String GIALLO_CHIARO = "\u001B[33m";
+    public static final String VERDE = "\u001B[32m";
+    public static final String VIOLA = "\u001B[35m";
+    public static final String CIANO = "\u001B[36m";
+    public static final String GIALLO = "\u001B[35m";
+    public static final String RESET = "\u001B[0m";
+    public static final String BLU = "\u001b[34m";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Client c = new Client("irene", "\u001B[34m");
-        c.connetti("localhost", 1906);
-        c.scrivi();
-        c.leggi();
-        c.chiudi();
+        Client c1 = new Client("irene", GIALLO_CHIARO);
+        c1.connetti("localhost", 1906);
+        c1.scrivi();
+        c1.leggi();
+        c1.chiudi();
     }
     
 }
